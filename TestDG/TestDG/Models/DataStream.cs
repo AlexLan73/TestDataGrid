232b252bc7421089ee604c0d6d0a0c9e860d9158
AlexLan73 <diving_73@mail.ru>
2023-10-18 17:20:40 +0300

    Start

---
+++ TestDG/TestDG/Models/DataStream.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using DynamicData;
+
+namespace TestDG.Models;
+
+public interface IDataContext
+{
+  DataStream DS { get; set; }
+}
+public class DataContext : IDataContext
+{
+  public DataStream DS { get; set; } = new ();
+}
+
+public class Human
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public int Arg { get; set; }
+  public float Value { get; set; }
+  public bool Is { get; set; }
+}
+
+public class DataStream
+{
+  public List<Human> LsHumans { get; set; }= new List<Human> ();
+  public SourceList<Human> Humans { get; set; } = new SourceList<Human>();
+  public SourceCache<Human, int> DHumans { get; set; } = new SourceCache<Human, int>(t => t.Id);
+  public DataStream()
+  {
+    Inicial();
+  }
+
+  public void Inicial()
+  {
+    LsHumans.Clear();
+    DHumans.Clear();
+    Humans.Clear();
+    LsHumans.Add(new Human() { Id = 0, Name = "Name0", Arg = 10, Value = 1f, Is = true });
+    LsHumans.Add(new Human() { Id = 1, Name = "Name1", Arg = 20, Value = 2f, Is = false });
+    LsHumans.Add(new Human() { Id = 2, Name = "Name2", Arg = 30, Value = 3f, Is = true });
+    LsHumans.Add(new Human() { Id = 3, Name = "Name3", Arg = 40, Value = 4f, Is = false });
+    LsHumans.Add(new Human() { Id = 4, Name = "Name4", Arg = 50, Value = 5f, Is = true });
+    LsHumans.Add(new Human() { Id = 5, Name = "Name5", Arg = 60, Value = 6f, Is = false });
+
+  }
+}
+
---
+++ TestDG/TestDG/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,61 @@
+﻿
+using System.Reactive.Linq;
+using DynamicData;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using TestDG.Models;
+
+using System;
+
+namespace TestDG.ViewModels;
+
+public class MainWindowViewModel : ReactiveObject
+{
+  #region ----  Title  ----
+  private string _title = "---!!!---   Test ---";
+  public string Title
+  {
+    get => _title;
+    set => this.RaiseAndSetIfChanged(ref _title, value);
+  }
+  #endregion
+
+  private ObservableCollection<Human> _obHuman = new();
+  public ObservableCollection<Human> ObHuman
+  {
+    get => _obHuman;
+    set => this.RaiseAndSetIfChanged(ref _obHuman, value);
+  }
+
+  private SourceCache<Human, int> _dHumans = new(t => t.Id);
+  public SourceCache<Human, int> DHumans
+  {
+    get => _dHumans;
+    set => this.RaiseAndSetIfChanged(ref _dHumans, value);
+  }
+
+
+  private readonly ReadOnlyObservableCollection<Human> _list;
+  public ReadOnlyObservableCollection<Human> TxList => _list;
+
+  [Obsolete]
+  public MainWindowViewModel()
+  {
+    var ds = new DataStream();
+    ObHuman = new ObservableCollection<Human>(ds.LsHumans.ToArray());
+    DHumans = new SourceCache<Human, int>(t => t.Id);
+
+    _ = DHumans.Connect()
+        .Filter(x=>x.Is==true)
+        .ObserveOnDispatcher()
+        .ObserveOn(RxApp.MainThreadScheduler)
+        .Bind(out _list)
+        .Subscribe();
+
+    ds.LsHumans.ForEach(x => DHumans.AddOrUpdate(x));
+
+
+  }
+
+}
+
